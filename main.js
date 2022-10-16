e7c2b3c78d5141334af6128727fdde4e788d7c19
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-10-15 21:25:31 -0300

    Funções anônimas.

--- main.js
+++ main.js
@@ -11,7 +11,9 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while(contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSom;
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom("#som_tecla_psh")
+    };
     contador++;
     console.log(contador);
 }
