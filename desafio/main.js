08b78c68d50db8fdba73be10bb13a7776ab2e92d
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-10-15 21:53:25 -0300

    Faça como eu fiz: Utilizando o For.

--- desafio/main.js
+++ desafio/main.js
@@ -1,4 +1,11 @@
-document.querySelector('input[type=tel]');
+const tel = document.querySelector('input[type=tel]');
 
 const listaDeTeclas = document.querySelectorAll('input[type=button]');
 console.log(listaDeTeclas);
+
+for (i = 0; i < listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i];
+    tecla.onclick = function() {
+        tel.value += tecla.value;
+    }
+}
