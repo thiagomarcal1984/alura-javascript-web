5c30f0e73ddea2c5766666a6ecbb880ecb89acef
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-10-15 21:00:11 -0300

    Percorrendo uma lista.

--- main.js
+++ main.js
@@ -8,4 +8,10 @@ function tocaSomClap () {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+while(contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador++;
+    console.log(contador);
+}
